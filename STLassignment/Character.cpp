97a8bd8463a1576a983e2e92f7d6b480ced04ea1
Elijah Evans <elijah.evans1@sinclair.edu>
2024-04-26 10:47:44 -0400

    Created Character.h with the throw functions. Created the Character.cpp with character checks which will call the throw funciton if there is an exception.

--- STLassignment/Character.cpp
+++ STLassignment/Character.cpp
@@ -5,6 +5,15 @@
 #include <cctype>
 
 char character(char start, int offset) {
+    if (!isalpha(start)) { // Check if letter
+        throw invalidCharacterException(); // Throws char exception if not
+    }
 
+    char result = start + offset;
 
+    if (!isalpha(result) || (isupper(start) && islower(result)) || (islower(start) && isupper(result))) { // Check if letter and is in the valid range
+        throw invalidRangeException(); // Throws exception if not
+    }
+
+    return result;
 }
--- STLassignment/Character.h
+++ STLassignment/Character.h
@@ -8,9 +8,20 @@
 #include <stdexcept>
 using namespace std;
 
+class invalidCharacterException : public exception {
+public:
+    const char* what() const throw () { // Throw the invalid char exception
+        return "Invalid character exception"; 
+    }
+};
 
+class invalidRangeException : public exception {
+public:
+    const char* what() const throw () { // Throw the invalid range exception
+        return "Invalid range exception";
+    }
+};
 
-
-
+char character(char start, int offset);
 
 #endif
